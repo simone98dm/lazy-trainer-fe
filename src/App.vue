a5ed3c7c65d1567c56d4c4d74dcb476d324f419d
simone98dm <simone.dalmas@outlook.it>
2022-09-13 21:23:52 +0200

    add icons, add page, improve code

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@ import Footer from "./components/Footer/Footer.vue";
 
 <template>
   <div class="mx-auto">
-    <div class="h-full bg-gray-50">
+    <div class="h-screen bg-gray-50">
       <Header></Header>
-      <router-view></router-view>
+      <div class="h-full">
+        <router-view></router-view>
+      </div>
       <Footer></Footer>
     </div>
   </div>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,17 +1,20 @@
 import Home from "../pages/Home.vue";
 import Details from "../pages/Details.vue";
 import Activity from "../pages/Activity.vue";
+import Setting from "../pages/Setting.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { name: "home", path: "/", component: Home },
   { name: "details", path: "/details/:id", component: Details },
   { name: "activity", path: "/activity/:dayOfWeek/:id", component: Activity },
+  { name: "setting", path: "/setting", component: Setting },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  linkExactActiveClass: "bg-slate-100 text-indigo-600",
 });
 
 export default router;
