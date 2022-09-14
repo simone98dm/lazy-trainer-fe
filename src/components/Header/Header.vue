97d34b553a2595e9738ee073a9fc572bf53129e5
simone98dm <simone.dalmas@outlook.it>
2022-09-14 07:22:03 +0200

    move header buttons

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,12 +4,12 @@ import ShareIcon from "../Icon/ShareIcon.vue";
 </script>
 
 <template>
-  <div class="fixed left-0 top-0 w-full rounded-b-xl bg-indigo-600 p-5 text-white">
+  <div class="w-full rounded-b-xl bg-indigo-600 p-5 text-white">
     <div class="flex items-center justify-between">
       <div
         class="rounded-lg bg-indigo-50/30 p-3 hover:bg-white hover:text-indigo-500"
       >
-        <menu-icon></menu-icon>
+        <share-icon></share-icon>
       </div>
       <h1 class="text-center text-3xl font-semibold">
         Trainer
@@ -17,7 +17,7 @@ import ShareIcon from "../Icon/ShareIcon.vue";
       <div
         class="rounded-lg bg-indigo-50/30 p-3 hover:bg-white hover:text-indigo-500"
       >
-        <share-icon></share-icon>
+        <menu-icon></menu-icon>
       </div>
     </div>
   </div>
