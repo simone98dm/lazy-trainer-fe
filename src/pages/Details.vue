dd0aa6f60520c89f7b84838fbf3f22333417d848
simone98dm <simone.dalmas@outlook.it>
2022-09-15 13:14:44 +0200

    fix details ui

--- src/pages/Details.vue
+++ src/pages/Details.vue
@@ -14,10 +14,11 @@ const route = useRoute();
 const store = useActivityStore();
 const queryParam = route.params.id;
 const activities = store.getSessionActivities(queryParam as string);
+const count = activities?.length ?? 0;
 </script>
 <template>
   <section class="flex flex-col justify-center">
-    <div class="w-full px-3 mb-5">
+    <div class="w-full px-3 mb-5" v-if="count > 0">
       <Link
         label="Run session"
         :type="LinkType.BUTTON"
@@ -27,24 +28,28 @@ const activities = store.getSessionActivities(queryParam as string);
         <Icon :component="PlayIcon" :size="IconSize.MEDIUM"></Icon>
       </Link>
     </div>
-    <h1 class="mb-3 text-2xl font-bold">List of activities:</h1>
-    <div v-for="activity in activities">
-      <Link
-        :to="{
-          name: 'activity',
-          params: { sessionId: queryParam, activityId: activity.id },
-        }"
-      >
-        <Item
-          :name="activity.name"
-          :description="activity.description"
-          :time="activity.time"
-          :id="activity.id"
-          :key="activity.id"
-        />
-      </Link>
+    <div v-if="count > 0">
+      <h1 class="mb-3 text-2xl font-bold">List of activities:</h1>
+      <div v-for="activity in activities">
+        <Link
+          :to="{
+            name: 'activity',
+            params: { sessionId: queryParam, activityId: activity.id },
+          }"
+        >
+          <Item
+            :name="activity.name"
+            :description="activity.description"
+            :time="activity.time"
+            :id="activity.id"
+            :key="activity.id"
+          />
+        </Link>
+      </div>
     </div>
-
+    <div v-else>
+      <h1 class="mb-3 text-2xl font-bold">No activity found</h1>
+    </div>
     <div class="w-full px-3 mb-5">
       <Link
         label="Add activity"
