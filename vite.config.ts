bd450dd571ae3b5f17ed5cd86b1d68c0b3058745
simone98dm <simone.dalmas@outlook.it>
2022-09-24 14:54:48 +0200

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        cleanupOutdatedCaches: false
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: 'Lazy Trainer',
+        short_name: 'Trainer',
+        description: 'Lazy Trainer tracker',
+        theme_color: '#4f46e5',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
