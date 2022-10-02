f53314ac8b681712e96ac2b49f004b0d31672593
simone98dm <simone.dalmas@outlook.it>
2022-10-02 17:36:14 +0200

    add route animation

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,26 @@
   <div class="min-h-screen bg-gray-50 flex flex-col">
     <Header></Header>
     <div class="p-5 w-full lg:w-4/5 mx-auto mb-14">
-      <router-view></router-view>
+      <router-view v-slot="{ Component, route }">
+        <transition name="fade" mode="out-in">
+          <div :key="(route.name as string)">
+            <component :is="Component" />
+          </div>
+        </transition>
+      </router-view>
     </div>
     <Footer v-if="!isLogin()"></Footer>
   </div>
   <GlobalLoading></GlobalLoading>
 </template>
+
+<style>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.1s ease;
+  }
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
