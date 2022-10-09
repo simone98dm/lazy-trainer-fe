e4650047c178ad645af81ba0e5378282d282f713
simone98dm <simone.dalmas@outlook.it>
2022-10-09 08:28:03 +0200

    wip

--- utils/token.ts
+++ utils/token.ts
@@ -2,11 +2,20 @@ import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "./const";
 import { ITokenPayload } from "../src/models/User";
 
+/**
+ * Verify token and return ITokenPayload interface
+ * @param token
+ * @returns ITokenPayload
+ */
 export function verifyToken(token: string): ITokenPayload | undefined {
   try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    if (decoded) {
-      return decoded as ITokenPayload;
+    if (token) {
+      const decoded = jwt.verify(token, SECRET_KEY);
+      if (decoded) {
+        return decoded as ITokenPayload;
+      }
+    } else {
+      throw new Error("Token is null");
     }
   } catch (error) {
     console.error(error);
