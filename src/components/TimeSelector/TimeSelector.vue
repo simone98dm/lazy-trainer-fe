46b6d01e7cef55b4d650c22c78d4411c23a7d785
simone98dm <simone.dalmas@outlook.it>
2022-10-25 21:52:19 +0200

    feat: :sparkles: create time selector component

---
+++ src/components/TimeSelector/TimeSelector.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps(["time", "hasError"]);
+  defineEmits(["timeSelected"]);
+</script>
+
+<template>
+  <Input
+    :value="time"
+    @change="(v) => $emit('timeSelected', v * 1000)"
+    id="activityTimer"
+    name="activityTimerField"
+    :has-error="hasError"
+    error="Time not valid"
+  />
+</template>
