55cc2430c9f9994395037f78e6140666c22c5088
simone98dm <simone.dalmas@outlook.it>
2022-10-25 21:52:02 +0200

    feat: :sparkles: create common input component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  defineProps(["value", "label", "hasError", "error", "id", "name", "type"]);
+  console.log();
+  defineEmits(["change"]);
+</script>
+
+<template>
+  <label
+    :class="['font-bold mb-2', { 'text-red-600': hasError }]"
+    for="activityName"
+  >
+    {{ label }}
+  </label>
+  <input
+    :value="value"
+    :class="[
+      'appearance-none block w-full bg-white border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none',
+      { 'border-red-600': hasError },
+    ]"
+    :name="name"
+    :id="id"
+    @change="$emit('change', (($event.target as any) || undefined)?.value)"
+    @input="$emit('change', (($event.target as any) || undefined)?.value)"
+    :type="type ?? 'text'"
+  />
+  <span v-if="hasError" class="text-red-600">{{ error }}</span>
+</template>
