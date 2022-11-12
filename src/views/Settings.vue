346940c2c7d6b01a4727cb21f3d36585a84cfb06
simone98dm <simone.dalmas@outlook.it>
2022-11-12 14:21:46 +0100

    improve ui settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -48,61 +48,94 @@
 <template>
   <div class="flex flex-col justify-center max-w-screen-lg mx-auto">
     <div class="shadow p-5 rounded-xl mb-6 bg-white">
-      <h1 class="text-xl font-bold mb-3">Profile</h1>
-      <div class="w-full p-3">
-        <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
-          Username
-          <span class="font-bold">
-            {{ userStore.username }}
-          </span>
-        </div>
-        <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
-          Role
-          <span class="font-bold">
-            {{ RoleName[userStore.role as Role] }}
-          </span>
-        </div>
-        <div
-          class="flex justify-between pb-1 mb-6 border-b-2 border-dotted"
-          v-if="!userStore.isTrainer && !userStore.isSelfMadeMan"
-        >
-          Trainer
-          <span class="font-bold" v-if="userStore.getTrainer">
-            {{ userStore.getTrainer.name }}
-          </span>
-          <Loading v-else :small="true"></Loading>
-        </div>
-      </div>
-      <h1 class="text-xl font-bold mb-3">Preferences</h1>
-      <div class="w-full p-3">
-        <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
-          Disable audio
-          <Switch
-            id="disableAudio"
-            name="toggleDisableAudio"
-            :checked="settingsStore.isAudioDisabled"
-            @toggle="toggleAudio"
-          />
-        </div>
-        <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
-          Easy mode
-          <Switch
-            id="easyMode"
-            name="toggleEasyMode"
-            :checked="settingsStore.isEasyModeEnabled"
-            @toggle="toggleEasyMode"
-          />
-        </div>
-        <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
-          App version
-          <span class="font-bold">
-            {{ version }}
-          </span>
+      <div class="m-6">
+        <h1 class="text-xl font-bold mb-3">Profile</h1>
+        <div class="w-full pt-3 px-3">
+          <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
+            Username
+            <span class="font-bold">
+              {{ userStore.username }}
+            </span>
+          </div>
+          <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
+            Role
+            <span class="font-bold">
+              {{ RoleName[userStore.role as Role] }}
+            </span>
+          </div>
+          <div
+            class="flex justify-between pb-1 mb-6 border-b-2 border-dotted"
+            v-if="!userStore.isTrainer && !userStore.isSelfMadeMan"
+          >
+            Trainer
+            <span class="font-bold" v-if="userStore.getTrainer">
+              {{ userStore.getTrainer.name }}
+            </span>
+            <Loading v-else :small="true"></Loading>
+          </div>
         </div>
       </div>
+      <div class="m-6">
+        <h1 class="text-xl font-bold mb-3">Preferences</h1>
+        <div class="w-full pt-3 px-3">
+          <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
+            Disable audio
+            <Switch
+              id="disableAudio"
+              name="toggleDisableAudio"
+              :checked="settingsStore.isAudioDisabled"
+              @toggle="toggleAudio"
+            />
+          </div>
+
+          <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
+            Easy mode
+            <Switch
+              id="easyMode"
+              name="toggleEasyMode"
+              :checked="settingsStore.isEasyModeEnabled"
+              @toggle="toggleEasyMode"
+            />
+          </div>
+
+          <div class="flex justify-between pb-1 mb-6 border-b-2 border-dotted">
+            App version
+            <span class="font-bold">
+              {{ version }}
+            </span>
+          </div>
+
+          <div class="flex justify-around">
+            <Link
+              @click="
+                logEvent(getAnalytics(), GaCustomEvents.CLICK, { to: 'author' })
+              "
+              :to="{ name: 'about' }"
+              :full="true"
+              :color="ButtonColor.LIGHT"
+              icon="account_circle"
+              label="Author"
+            />
+            <Link
+              @click="
+                logEvent(getAnalytics(), GaCustomEvents.CLICK, {
+                  to: 'licence',
+                })
+              "
+              :to="{ name: 'license' }"
+              :full="true"
+              :color="ButtonColor.LIGHT"
+              icon="verified_user"
+              label="License"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full flex justify-center mb-3">
       <Button
         v-if="!userStore.isTrainer"
-        :color="ButtonColor.PRIMARY"
+        :color="ButtonColor.LIGHT"
         :loading="syncStatus"
         full="true"
         icon="cloud_sync"
@@ -110,31 +143,7 @@
         @click="syncProfile"
       />
     </div>
-    <div class="mb-6 flex justify-around gap-4">
-      <Link
-        @click="
-          logEvent(getAnalytics(), GaCustomEvents.CLICK, { to: 'author' })
-        "
-        :to="{ name: 'about' }"
-        :full="true"
-        :color="ButtonColor.LIGHT"
-        icon="account_circle"
-        label="Author"
-      >
-      </Link>
-      <Link
-        @click="
-          logEvent(getAnalytics(), GaCustomEvents.CLICK, { to: 'licence' })
-        "
-        :to="{ name: 'license' }"
-        :full="true"
-        :color="ButtonColor.LIGHT"
-        icon="verified_user"
-        label="License"
-      >
-      </Link>
-    </div>
-    <div class="w-full flex justify-center">
+    <div class="w-full flex justify-center mb-3">
       <Button
         :color="ButtonColor.PRIMARY"
         full="true"
