60fff5f4bde5f702099534160f350fc4d5f5f1ac
simone98dm <simone.dalmas@outlook.it>
2022-12-01 08:53:56 +0100

    add v-calendar and init on component

---
+++ src/components/Calendar/Calendar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const attr = [
+    {
+      key: "today",
+      highlight: {
+        class: "bg-indigo-500",
+        fillMode: "outline",
+      },
+      dates: new Date(),
+    },
+    {
+      key: "completed",
+      dot: true,
+      dates: new Date(2022, 11, 2),
+    },
+  ];
+</script>
+
+<template>
+  <v-calendar is-expanded :attributes="attr" />
+</template>
